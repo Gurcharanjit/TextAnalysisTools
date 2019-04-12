fae9f58c3e4cd3837350b6d1744aebcb19426cd5
visualstudio <visualstudio@122-PC13>
2019-04-12 19:41:26 -0400

    FINAL CODE

--- assignment 5 second part/Program.cs
+++ assignment 5 second part/Program.cs
@@ -10,6 +10,34 @@ namespace assignment_5_second_part
     {
         static void Main(string[] args)
         {
+
+            Program a = new Program();
+
+            Console.WriteLine("the longest word is : ");
+
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
+
+            Console.ReadLine();
         }
+        public String MyFunctionA(string input)
+        {
+            string[] words = input.Split(' ');
+            int wordArrayLength = words.Length;
+            int[] wordsLength = new int[wordArrayLength];
+        
+            foreach (var word in words)
+            {
+            }
+        
+            string currentWord =   words[0];
+            string nextWord;
+            for (int y = 0; y < words.Length - 1; y++)
+            {
+                currentWord = words[y];
+                nextWord = words[y + 1];
+            }
+            return currentWord;
+        }
+
     }
 }
